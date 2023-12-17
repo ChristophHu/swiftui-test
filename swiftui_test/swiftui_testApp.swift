8f78e77214afe8017a1239014a1944e89ff24bc6
Christoph Hu <christoph.huschenhoefer@gmx.de>
2023-12-17 18:50:04 +0100

    Initial Commit

---
+++ swiftui_test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui_test
+//
+//  Created by Christoph on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
